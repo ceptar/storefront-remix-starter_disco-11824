d02af13dece29eb17671dabdb919309c3b0cfbc9
ceptar <christoph.cerjan@gmail.com>
2024-09-08 16:36:57 +0000

    cc

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -202,9 +202,11 @@ export default function Carousel({ featuredProducts }) {
           <div
             key={index} // Prefer using a unique product identifier here
             className="relative flex-shrink-0 flex flex-col items-center justify-center b-radius-0 
-            w-[calc(100vw-48px)] sm:w-[calc(50vw-32px)] lg:w-[calc(25vw-24px)]
+            w-[calc(50vw-24px)] sm:w-[calc(25vw-20px)]
             "
- /* darüber nach " einsetzen!         w-[calc(100vw-30px)] sm:w-[calc(50vw-25px)] lg:w-[calc(33vw-20px)] */
+ /* 
+ w-[calc(100vw-48px)] 
+ darüber nach " einsetzen!         w-[calc(100vw-30px)] sm:w-[calc(50vw-25px)] lg:w-[calc(33vw-20px)] */
 //mit diesen größen lässt sich die anzahl an cards je nach zb screengröße einstellen
 
           >
