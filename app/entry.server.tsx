0ae5cf71a8de41b3c790594c5972aebbb6646a26
ceptar <christoph.cerjan@gmail.com>
2024-11-20 23:36:46 +0000

    cc

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,11 @@
-import { renderToString } from 'react-dom/server';
-import { EntryContext } from '@remix-run/server-runtime';
+import { renderToPipeableStream } from 'react-dom/server';
+import { PassThrough } from 'stream';
+import isbot from 'isbot';
+import { EntryContext, HandleDataRequestFunction } from '@remix-run/server-runtime';
 import { RemixServer } from '@remix-run/react';
+import { cors } from "remix-utils/cors";
+
+const ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -8,14 +13,57 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />,
-  );
+	let callbackName = isbot(request.headers.get("user-agent"))
+		? "onAllReady"
+		: "onShellReady";
 
-  responseHeaders.set('Content-Type', 'text/html');
+	return new Promise((resolve, reject) => {
+		let didError = false;
 
-  return new Response('<!DOCTYPE html>' + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
-  });
+		let { pipe, abort } = renderToPipeableStream(
+			<RemixServer context={remixContext} url={request.url} />,
+			{
+				[callbackName]: () => {
+					let body = new PassThrough();
+
+					responseHeaders.set("Content-Type", "text/html");
+
+					cors(
+						request,
+						new Response(new ReadableStream({
+							start(controller) {
+								body.on('data', chunk => controller.enqueue(chunk));
+								body.on('end', () => controller.close());
+								body.on('error', err => controller.error(err));
+							}
+						}), {
+							headers: responseHeaders,
+							status: didError ? 500 : responseStatusCode,
+						}),
+					).then((response) => {
+						resolve(response);
+					});
+
+					pipe(body);
+				},
+				onShellError: (err: unknown) => {
+					reject(err);
+				},
+				onError: (error: unknown) => {
+					didError = true;
+
+					console.error(error);
+				},
+			},
+		);
+
+		setTimeout(abort, ABORT_DELAY);
+	});
 }
+
+export let handleDataRequest: HandleDataRequestFunction = async (
+	response,
+	{ request },
+) => {
+	return await cors(request, response);
+};
